12a1e42d309b9d3f9b7e2fe3b770f5e825b01540
GAFAR007 <temitayogafar@gmail.com>
2023-08-29 15:14:03 +0100

    ...

--- js-thedom-P1/__workshop/_fundamentals/fundamentals-1.js
+++ js-thedom-P1/__workshop/_fundamentals/fundamentals-1.js
@@ -27,5 +27,5 @@ const calculator = (arr) => {
 // Look for the corresponding exercise file in the __tests__ folder.
 // Add some test cases in the test. The first one is done for you.
 
-// We need to export the function in order for our unit test to have access to it.
+// We need to export the function in order for our unit test to have access to it...
 module.exports = calculator;
